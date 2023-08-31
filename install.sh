72072e8b8534ac7546000bdf4dde7807bfd00916
xgwang9 <39149138+xgwang9@users.noreply.github.com>
2023-08-31 12:32:31 -0500

    Change back to ohmyzsh

--- install.sh
+++ install.sh
@@ -3,12 +3,16 @@
 sudo apt install zsh -y
 chsh -s $(which zsh)
 
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.zsh/powerlevel10k
+#git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.zsh/powerlevel10k
+#echo 'source ~/.zsh/powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
+
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
 git clone --depth=1 https://github.com/zsh-users/zsh-history-substring-search.git ~/.zsh/zsh-history-substring-search
-echo 'source ~/.zsh/powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
 echo 'source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh' >> ~/.zshrc
 echo 'source ~/.zsh/zsh-history-substring-search/zsh-history-substring-search.zsh' >> ~/.zshrc
+sed -i 's/robbyrussell/avit/g' ~/.zshrc
 
 echo >> ~/.zshrc
 echo 'HISTFILE=~/.zsh_history' >> ~/.zshrc
